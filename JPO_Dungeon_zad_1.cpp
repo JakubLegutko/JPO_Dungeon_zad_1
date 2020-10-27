3d42ea9caf006a36071c0c7f5971fdc333d3ced2
jlegutko@student.agh.edu.pl <jlegutko@student.agh.edu.pl>
2020-10-27 19:17:24 +0100

    refactored code

--- JPO_Dungeon_zad_1.cpp
+++ JPO_Dungeon_zad_1.cpp
@@ -3,12 +3,45 @@
 #include <conio.h>
 #include <cmath>
 #include<time.h>
+#include<vector>
 
-char map[9][9];
+#define MAP_ROWS 9
+#define MAP_COLS 9
+int Py = 0;
+int Pxn = 0;
+int Pyn = 0;
+const int X1y = 2;
+const int X2y = 3;
+const int X3y = 4;
+const int X4y = 6;
+const int Ty = 8;
+int Tx;
+int Px;
+int X1x;
+int X2x;
+int X3x;
+int X4x;
+int X1xm;
+int X2xm;
+int X3xm;
+int X4xm;
+using std::vector;
+
+
+
+vector<vector<char>> map;
+
+void map_init() {
+    for (int i = 0; i < MAP_COLS; i++) {
+        for (int j = 0; j < MAP_ROWS; j++) {
+            map.resize(MAP_ROWS, vector<char>(MAP_COLS, '.'));
+        }
+    }
+}
 void draw_no_clear() {
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < MAP_COLS; i++) {
         printf("\n");
-        for (int j = 0; j < 9; j++) {
+        for (int j = 0; j < MAP_ROWS; j++) {
             printf("%c", map[i][j]);
         }
     }
@@ -16,192 +49,175 @@ void draw_no_clear() {
 }
 void draw() {
     system("cls");
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < MAP_COLS; i++) {
         printf("\n");
-        for (int j = 0; j < 9; j++) {
+        for (int j = 0; j < MAP_ROWS; j++) {
             printf("%c", map[i][j]);
         }
     }
     printf("\n");
 }
+void prep_map() {
+    map[X1y][X1x] = 'X';
+    map[X2y][X2x] = 'X';
+    map[X3y][X3x] = 'X';
+    map[X4y][X4x] = 'X';
+    map[Py][Px] = 'P';
+    map[Ty][Tx] = 'T';
+}
+void randomize_game() {
+    Tx = rand() % 10;
+    Px = rand() % 10;
+    X1x = rand() % 10;
+    X2x = rand() % 10;
+    X3x = rand() % 10;
+    X4x = rand() % 10;
+}
+void win_sequence() {
+    printf("Kongratulejszyn, wygrales wlasnie Gre!\n");
+    Sleep(1000);
+    printf("....\n");
+    Sleep(2000);
+    printf("........\n");
+    Sleep(500);
+    printf("Co, czekasz na nagrode?\n");
+    Sleep(400);
+    printf("Nie slyszales? Kryzys jest!\n");
+    Sleep(2000);
+    printf("Ehh, no dobra, cos tam sie znajdzie...");
+    Sleep(1000);
+    printf("https://www.youtube.com/watch?v=G1IbRujko-A&t=17s");
+    printf("\n");
+    Sleep(300);
+    printf("Milej pracy!!!");
+    Sleep(5000);
+    map.clear();
+    exit(0);
+}
+void lose_sequence() {
+    prep_map();
+    draw();
+    printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
+    Sleep(2000);
+    map.clear();
+    exit(0);
+}
+void refresh_map() {
+    map[X1y][X1x] = '.';
+    map[X2y][X2x] = '.';
+    map[X3y][X3x] = '.';
+    map[X4y][X4x] = '.';
+    map[Py][Px] = '.';
+
+
+    X1x = abs(X1x + X1xm);
+    X2x = abs(X2x + X2xm);
+    X3x = abs(X3x + X3xm);
+    X4x = abs(X4x + X4xm);
+    Py = Pyn;
+    Px = Pxn;
+}
+void randomize_X() {
+    X1xm = rand() % 3;
+    X2xm = rand() % 3;
+    X3xm = rand() % 3;
+    X4xm = rand() % 3;
+    X1xm -= 3;
+    X2xm -= 3;
+    X3xm -= 3;
+    X4xm -= 3;
+}
+int move(char input) {
+    switch (input) {
+    case 'w':
+        Pyn = Py - 1;
+        if (Pyn < 0) {
+            printf("Ojj ktos tu wyszedl poza mape, ded");
+            Sleep(2000);
+            map.clear();
+            exit(0);
+        }
+        return 1;
+    case 's':
+        Pyn = Py + 1;
+        Pxn = Px;
+        if (Pyn > 8) {
+            printf("Ej szefie, nie za daleko sie to poszlo?");
+            Sleep(2000);
+            map.clear();
+            exit(0);
+        }
+        return 1;
+    case 'a':
+        Pxn = Px - 1;
+        if (Pxn < 0) {
+            printf("Biedaczku tak sie starales a z gry wyleciales :(");
+            Sleep(2000);
+            map.clear();
+            exit(0);
+        }
+        return 1;
+    case 'd':
+        Pxn = Px + 1;
+        if (Pxn > 9) {
+            printf("Niektorzy mowia:tak blisko. Ja wole stwierdzenie zle.");
+            Sleep(2000);
+            map.clear();
+            exit(0);
+        }
+        return 1;
+    case 'x':
+        printf("Konczenie gry ....\n");
+        printf("....................");
+        Sleep(1000);
+        map.clear();
+        exit(0);
+    default:
+        printf("Ojjjj nie nie byczq, ten klawisz to nie input, ale jestem mily i dam Ci jeszcze jedna szanse!\n");
+        return 3;
+    }
+}
 int main()
 {
-    srand(time(NULL));
+    srand(time(0));
     
     std::cout << "Witaj w Grze!\n";
     std::cout << "Ruszasz sie WASD, baw sie dobrze, T to skarb, chcesz to chlopcze, X cię zamorduje nie dotykaj, P to Ty!, naciskajac x konczysz gre :(\n";
-    int Tx = rand() % 10;
-    int Ty = 8;
-    int Px = rand() % 10;
-    int Py = 0;
-    int Pxn = 0;
-    int Pyn = 0;
-    int X1y = 2;
-    int X2y = 3;
-    int X3y = 4;
-    int X4y = 6;
-    int X1x = rand() % 10;
-    int X2x = rand() % 10;
-    int X3x = rand() % 10;
-    int X4x = rand() % 10;
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            map[i][j] = '.'; 
-        }
-    }
-    map[2][X1x] = 'X';
-    map[3][X2x] = 'X';
-    map[4][X3x] = 'X';
-    map[6][X4x] = 'X';
-    map[0][Px] = 'P';
-    map[8][Tx] = 'T';
+    randomize_game();
+    map_init();
+    prep_map();
     draw_no_clear();
     printf("\nTak wyglada Twoja plansza gry, wyjdz poza nia, a skonczy sie to zle!\n");
 
     while (1)
     {
         if (Px == Tx && Py == Ty) {
-            printf("Kongratulejszyn, wygrales wlasnie Gre!\n");
-            Sleep(1000);
-            printf("....\n");
-            Sleep(2000);
-            printf("........\n");
-            Sleep(500);
-            printf("Co, czekasz na nagrode?\n");
-            Sleep(400);
-            printf("Nie slyszales? Kryzys jest!\n");
-            Sleep(2000);
-            printf("Ehh, no dobra, cos tam sie znajdzie...");
-            Sleep(1000);
-            printf("https://www.youtube.com/watch?v=G1IbRujko-A&t=17s");
-            printf("\n");
-            Sleep(300);
-            printf("Milej pracy!!!");
-            Sleep(5000);
-            exit(0);
+            win_sequence();
         }
-        else if (Px == (X1x || X2x || X3x || X4x) && Py == (X1y || X2y || X3y || X4y)) {
-            printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
-            Sleep(1000);
-            exit(1);
-            }
         else {
-            int X1xm = rand() % 3;
-            int X2xm = rand() % 3;
-            int X3xm = rand() % 3;
-            int X4xm = rand() % 3;
-            X1xm -= 3;
-            X2xm -= 3;
-            X3xm -= 3;
-            X4xm -= 3;
+            randomize_X();
        
             char input = _getch();
-            switch (input) {
-                case 'w' :
-                    Pyn = Py - 1;
-                    if (Pyn < 0) {
-                        printf("Ojj ktos tu wyszedl poza mape, ded");
-                        Sleep(2000);
-                        exit(1);
-                    }
-                    break;
-                case 's' :
-                    Pyn = Py + 1;
-                    Pxn = Px;
-                    if (Pyn > 8) {
-                        printf("Ej szefie, nie za daleko sie to poszlo?");
-                        Sleep(2000);
-                        exit(1);
-                    }
-                    break;
-                case 'a' :
-                    Pxn = Px - 1;
-                    if (Pxn < 0) {
-                        printf("Biedaczku tak sie starales a z gry wyleciales :(");
-                        Sleep(2000);
-                        exit(1);
-                    }
-                    break;
-                case 'd':
-                    Pxn = Px + 1;
-                    if (Pxn > 9) {
-                        printf("Niektorzy mowia:tak blisko. Ja wole stwierdzenie zle.");
-                        Sleep(2000);
-                        exit(1);
-                    }
-                    break;
-                case 'x':
-                    printf("Konczenie gry ....\n");
-                    printf("....................");
-                    Sleep(1000);
-                    exit(1);
-                default:
-                    printf("Ojjjj nie nie byczq, ten klawisz to nie input, ale jestem mily i dam Ci jeszcze jedna szanse!\n");
-                    continue;
+            switch (move(input)) {
+            case 1: break;
+            case 3: continue;
             }
-            map[X1y][X1x] = '.';
-            map[X2y][X2x] = '.';
-            map[X3y][X3x] = '.';
-            map[X4y][X4x] = '.';
-            map[Py][Px] = '.';
 
-
-            X1x = abs( X1x + X1xm);
-            X2x = abs(X2x + X2xm);
-            X3x =abs (X3x + X3xm);
-            X4x = abs(X4x + X4xm);
-            Py = Pyn;
-            Px = Pxn;
+            refresh_map();
             if (Px == (X1x) && Py == (X1y)) {
-                map[X1y][X1x] = 'X';
-                map[X2y][X2x] = 'X';
-                map[X3y][X3x] = 'X';
-                map[X4y][X4x] = 'X';
-                map[Py][Px] = 'P';
-                draw();
-                printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
-                Sleep(500);
-                exit(1);
+                lose_sequence();
             }
             if (Px == (X2x) && Py == (X2y)) {
-                map[X1y][X1x] = 'X';
-                map[X2y][X2x] = 'X';
-                map[X3y][X3x] = 'X';
-                map[X4y][X4x] = 'X';
-                map[Py][Px] = 'P';
-                draw();
-                printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
-                Sleep(500);
-                exit(1);
+                lose_sequence();
             }
             if (Px == (X3x) && Py == (X3y)) {
-                map[X1y][X1x] = 'X';
-                map[X2y][X2x] = 'X';
-                map[X3y][X3x] = 'X';
-                map[X4y][X4x] = 'X';
-                map[Py][Px] = 'P';
-                draw();
-                printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
-                Sleep(500);
-                exit(1);
+                lose_sequence();
             }
             if (Px == (X4x) && Py == (X4y)) {
-                map[X1y][X1x] = 'X';
-                map[X2y][X2x] = 'X';
-                map[X3y][X3x] = 'X';
-                map[X4y][X4x] = 'X';
-                map[Py][Px] = 'P';
-                draw();
-                printf("Tak bardzo sie starales ale w iksa sie wpakowales \n");
-                Sleep(500);
-                exit(1);
+                lose_sequence();
             }
 
-            map[X1y][X1x] = 'X';
-            map[X2y][X2x] = 'X';
-            map[X3y][X3x] = 'X';
-            map[X4y][X4x] = 'X';
-            map[Py][Px] = 'P';
+            prep_map();
             draw();
             
         }
